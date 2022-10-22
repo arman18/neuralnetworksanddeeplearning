9d673914319a231de8208002a541ee282af97b32
arman18 <bsse1029@iit.du.ac.bd>
2022-10-22 21:34:42 +0600

    task1_added

--- tasks1_hlayer_rate_activator_changing/activators.py
+++ tasks1_hlayer_rate_activator_changing/activators.py
@@ -1,7 +1,49 @@
+
+# https://medium.com/@omkar.nallagoni/activation-functions-with-derivative-and-python-code-sigmoid-vs-tanh-vs-relu-44d23915c1f4
+
+import numpy as np
+
+# ----------------------- sigmoid --------------------------------
+
 def sigmoid(z):
-    """The sigmoid function."""
     return 1.0/(1.0+np.exp(-z))
 
-def sigmoid_prime(z):
-    """Derivative of the sigmoid function."""
+def dsigmoid_prime(z):
     return sigmoid(z)*(1-sigmoid(z))
+
+# ----------------------- Tanh --------------------------------
+def tanh(x):
+    
+    return (np.exp(x)-np.exp(-x))/(np.exp(x)+np.exp(-x))
+
+def dtanh(x):
+    return 1-tanh(x)**2
+
+# ----------------------- ReLu --------------------------------
+def relu(x):
+  return np.maximum(0.,x)
+
+def drelu(x):
+  return np.greater(x, 0.).astype(np.float32)
+
+# ----------------------- softmax --------------------------------
+def softmax(x):
+    """ applies softmax to an input x"""
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum()
+
+def dsoftmax(x):
+    s = softmax(x)
+    si_sj = - s * s.reshape(3, 1)
+    return np.diag(s) + si_sj
+
+# ----------------------- Tanh --------------------------------
+# ----------------------- Tanh --------------------------------
+# ----------------------- Tanh --------------------------------
+# ----------------------- Tanh --------------------------------
+# ----------------------- Tanh --------------------------------
+# ----------------------- Tanh --------------------------------
+# ----------------------- Tanh --------------------------------
+# ----------------------- Tanh --------------------------------
+# ----------------------- Tanh --------------------------------
+# ----------------------- Tanh --------------------------------
--- tasks1_hlayer_rate_activator_changing/main.py
+++ tasks1_hlayer_rate_activator_changing/main.py
@@ -1,6 +1,21 @@
-import mnist_loader
-training_data , validation_data , test_data = mnist_loader . load_data_wrapper ()
+# from ../base_codes.mnist_loader 
+import os, sys
+sys.path.append(os.path.dirname(os.path.realpath("../base_codes/mnist_loader.py")))
+# sys.path.append(os.path.dirname(os.path.realpath("../base_codes/mnist.pkl.gz")))
 
+# from 
+import mnist_loader
+
+training_data , validation_data , test_data = mnist_loader.load_data_wrapper()
+
+from network import Network
+from activators import *
+import pandas as pd
+
+
+epochs = 30
+mini_batch_size = 10
+eta = 3.0 #learning rate (need to change)
 
 neurons_in_hidden_layer = 30
 neurons_in_output  = 10
@@ -8,9 +23,11 @@ neurons_in_input = 784
 max_hidden_layer = 20
 max_rate = 10
 obj = {}
-activators = [sigmoid]
-str_activators = ['sigmoid']
-derivatives = [sigmoid_prime]
+
+activators = [relu,sigmoid,tanh,softmax]
+derivatives = [drelu,dsigmoid_prime,dtanh,dsoftmax]
+str_activators = ['relu','sigmoid','tanh','softmax']
+
 for rate in range(1,max_rate+1):
   obj[rate] = pd.DataFrame()
   obj[rate]["layer"] = [0]*max_hidden_layer
@@ -19,6 +36,7 @@ for rate in range(1,max_rate+1):
   obj[rate]["relu"] = [0]*max_hidden_layer
   obj[rate]["softmax"] = [0]*max_hidden_layer
   obj[rate]["relu"] = [0]*max_hidden_layer
+  
   for layer in range(1,max_hidden_layer+1):
     obj[rate]["layer"][layer-1] = layer
     for ac in range(len(activators)):
